0b2b70d5b4ee16886fcf686869180c6028dd8e82
Assia-Aissa <assiaboussanni2003aissa.com>
2024-07-30 12:29:13 +0100

    modify the dependencies + add an enum for multiple roles

---
+++ src/main/java/com/pfe/project/modeles/ERole.java
@@ -0,0 +1,10 @@
+package com.pfe.project.modeles;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ETUDIANT,
+    ROLE_RESPONSABLE_PFE,
+    ROLE_ENCADRANT,
+    ROLE_RESPONSABLE_GROUP,
+    ROLE_ADMIN
+}
